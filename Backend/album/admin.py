dc17e13b39d3118b6d6cc19f0ecc68c3386f658b
Leonardo Dantas <l.dantas1155@gmail.com>
2025-03-22 21:15:26 -0300

    feat: created admin's for Tag e Album

--- Backend/album/admin.py
+++ Backend/album/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 
-from album.models import Album
+from album.models import Album, Tag
 
 
 class AlbumAdmin(admin.ModelAdmin):
     model = Album
 
-    list_display = ('id_album', 'name_album', 'photographer', 'created_at')
+    list_display = ('id', 'name_album', 'photographer', 'created_at_album')
 
-    search_fields = ('description', 'month')
-    ordering = ('id_album')
+    search_fields = ('name_album',)
+    ordering = ('id',)
 
 admin.site.register(Album, AlbumAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+    list_display = ('id', 'name_tag', 'photographer')
+
+    search_fields = ('name_tag',)
+    ordering = ('id', 'name_tag')
+
+admin.site.register(Tag, TagAdmin)
