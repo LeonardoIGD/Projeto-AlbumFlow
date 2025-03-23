156b6dd01cff2d74e7970ac6e4bdc7e1e1d03061
Leonardo Dantas <l.dantas1155@gmail.com>
2025-03-22 21:18:01 -0300

    feat: creating admin for photographer

--- Backend/user/admin.py
+++ Backend/user/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 from .models.profile import Photographer
+class PhotographerAdmin(admin.ModelAdmin):
+    model = Photographer
 
-admin.site.register(Photographer)
+    list_display = ('id', 'name_photographer', 'name_company', 'phone')
+
+    search_fields = ('name_photographer', 'name_company')
+    ordering = ('name_photographer',)
+
+admin.site.register(Photographer, PhotographerAdmin)
