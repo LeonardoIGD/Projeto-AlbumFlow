d9a72bdafc2c0652c716a13667cc12b0e7b0a128
Leonardo Dantas <l.dantas1155@gmail.com>
2025-03-23 16:03:56 -0300

    feat: adding routes for Album and Tag

---
+++ Backend/album/api/__init__.py
@@ -0,0 +1,7 @@
+# Serializers
+from .serializers.album_serializer import AlbumSerializer
+from .serializers.tag_serializar import TagSerializer
+
+# Viewsets
+from .viewsets.album_viewset import AlbumViewSet
+from .viewsets.tag_viewset import TagViewSet
---
+++ Backend/album/api/viewsets/tag_viewset.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets, permissions
+from album.models import Tag
+from ..serializers.tag_serializar import TagSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
