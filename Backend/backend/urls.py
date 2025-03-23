9a0c2f373dcda5ad77c00190906f3e300e66b2c9
Leonardo Dantas <l.dantas1155@gmail.com>
2025-03-22 21:22:11 -0300

    remove: removing route that does not exist

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('admin/', admin.site.urls)
 ]
