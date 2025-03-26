6ca7079f9e9ff63399e3abdb1565f5dfd70c0071
Leonardo Dantas <l.dantas1155@gmail.com>
2025-03-26 16:01:28 -0300

    fix: correcting the request and use of user information in the frontend

--- Backend/user/api/serializers/photographer_serializer.py
+++ Backend/user/api/serializers/photographer_serializer.py
@@ -4,10 +4,13 @@ from user.models import Photographer
 
 
 class PhotographerSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+
     class Meta:
         model = Photographer
-        fields = ['user', 'type_user', 'name_photographer', 'name_company']
-        read_only_fields = ['id']
+        fields = ['user', 'type_user', 'username', 'email', 'phone', 'name_photographer', 'name_company']
+        read_only_fields = ['id', 'username', 'email']
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
