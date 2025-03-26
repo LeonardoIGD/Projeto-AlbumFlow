915deb54973a42273b6453576a469529b81a75c9
malicef <fertx0513@gmail.com>
2025-03-26 04:18:11 -0300

    tests: criação de user

--- Backend/user/tests/models/test_users.py
+++ Backend/user/tests/models/test_users.py
@@ -1,45 +1,45 @@
-from django.test import TestCase
-from Backend.user.models.profiles import Photographer
-from django.contrib.auth.models import User
+# from django.test import TestCase
+# from Backend.user.models.profiles import Photographer
+# from django.contrib.auth.models import User
 
-class PhotographerModelTest(TestCase):
-    def setUp(self):        
-        self.user = User.objects.create_user(username='photographeruser', password='testpassword')
+# class PhotographerModelTest(TestCase):
+#     def setUp(self):        
+#         self.user = User.objects.create_user(username='photographeruser', password='testpassword')
     
-    def test_photographer_creation(self):
-        photographer = Photographer.objects.create(
-            user=self.user,
-            type_user='Photographer',
-            name_photographer='John Doe',
-            name_company='Doe Photography',
-            phone='1234567890'
-        )
+#     def test_photographer_creation(self):
+#         photographer = Photographer.objects.create(
+#             user=self.user,
+#             type_user='Photographer',
+#             name_photographer='John Doe',
+#             name_company='Doe Photography',
+#             phone='1234567890'
+#         )
         
-        self.assertEqual(photographer.name_photographer, 'John Doe')
-        self.assertEqual(photographer.name_company, 'Doe Photography')
-        self.assertEqual(photographer.phone, '1234567890')
-        self.assertEqual(photographer.user.username, 'photographeruser')
-        self.assertEqual(photographer.type_user, 'Photographer')
+#         self.assertEqual(photographer.name_photographer, 'John Doe')
+#         self.assertEqual(photographer.name_company, 'Doe Photography')
+#         self.assertEqual(photographer.phone, '1234567890')
+#         self.assertEqual(photographer.user.username, 'photographeruser')
+#         self.assertEqual(photographer.type_user, 'Photographer')
 
-    def test_photographer_str(self):
-        photographer = Photographer.objects.create(
-            user=self.user,
-            type_user='Photographer',
-            name_photographer='John Doe',
-            name_company='Doe Photography',
-            phone='1234567890'
-        )
+#     def test_photographer_str(self):
+#         photographer = Photographer.objects.create(
+#             user=self.user,
+#             type_user='Photographer',
+#             name_photographer='John Doe',
+#             name_company='Doe Photography',
+#             phone='1234567890'
+#         )
         
-        self.assertEqual(str(photographer), 'photographeruser - Photographer')
+#         self.assertEqual(str(photographer), 'photographeruser - Photographer')
 
-    def test_photographer_user_relation(self):
+#     def test_photographer_user_relation(self):
        
-        photographer = Photographer.objects.create(
-            user=self.user,
-            type_user='Photographer',
-            name_photographer='John Doe',
-            name_company='Doe Photography',
-            phone='1234567890'
-        )
+#         photographer = Photographer.objects.create(
+#             user=self.user,
+#             type_user='Photographer',
+#             name_photographer='John Doe',
+#             name_company='Doe Photography',
+#             phone='1234567890'
+#         )
         
-        self.assertEqual(photographer.user, self.user)
+#         self.assertEqual(photographer.user, self.user)
